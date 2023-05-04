8bcba7261cf61a06b692e4ced5ff051c671c2270
Cristian <77325773+CristianSivilla@users.noreply.github.com>
2023-05-04 19:30:19 +0200

    submenus implementados

--- models/lista.py
+++ models/lista.py
@@ -7,3 +7,4 @@ class Lista(models.Model):
     id_lista = fields.Integer(string='Id_Lista', required=True, index=True, help='Id de la lista')
     nom = fields.Char(string='Nom', help='Nombre de la lista')
     lista = fields.Many2one('hr.employee', string='Empleado de la lista', required=True, domain="[('active','=',True)]")
+    canciones = fields.One2many('cancion', 'id_lista', string='Canciones')
