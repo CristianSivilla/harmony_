01782ccf27373b6c4ce852fa73a00c0d690591d7
Cristian <77325773+CristianSivilla@users.noreply.github.com>
2023-05-10 12:00:44 +0200

    Version Estable

--- models/artista.py
+++ models/artista.py
@@ -4,7 +4,7 @@ class Artista(models.Model):
     _name = 'artista'
     _description = 'Modelo de artista'
 
-    id_artista = fields.Integer(string='Id_Artista', required=True, index=True, help='Id del artista', default=1, store=True)
+    id_artista = fields.Integer(string='Id_Artista', help='Id del artista')
     nombre = fields.Char(string='Nombre', required=True, help='Nombre del artista')
     nacionalidad = fields.Char(string='Nacionalidad', help='Nacionalidad del artista')
     
